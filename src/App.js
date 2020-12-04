09e189b761c7a044477cd6e7da3b15f1846317f2
patwang123 <patrickwang86@gmail.com>
2020-12-04 01:33:25 -0500

    dropdown button

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
     <div id='begin'>
       <div className='background'></div>
       <div className='dropdown'>
-        <button className="dropbtn">
-          <FontAwesomeIcon icon={faBars} size='2x' style={{color:'white'}} className='nav'></FontAwesomeIcon>
+        <button className="dropbtn" onClick={() => drop_down()} id='dropbtn'>
+          <FontAwesomeIcon icon={faBars} size='2x' style={{color:'white',pointerEvents: 'none'}} className='nav' ></FontAwesomeIcon>
         </button>
-        <div className="dropdown-content">
+        <div className="dropdown-content" id='dropdown-content'>
           <a href="#begin" onClick={(event) => { event.preventDefault(); document.getElementById('begin').scrollIntoView()}}>Intro</a>
           <a href="#about-me" onClick={(event) => { event.preventDefault(); document.getElementById('about-me').scrollIntoView() }}>About Me</a>
           <a href="#projects" onClick={(event) => { event.preventDefault(); document.getElementById('projects').scrollIntoView() }}>Projects</a>
@@ -33,6 +33,27 @@ function App() {
       </div>
     </div>
   );
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function drop_down() {
+  document.getElementById("dropdown-content").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  console.log(event.target)
+  if (event.target.id !== 'dropbtn' && event.target.id !== 'dropdown-content') {
+    console.log('hoi!')
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
 
 export default App;
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,9 +8,6 @@ export default function About () {
             <p>
                 I'm Patrick Wang, a sophomore at UC Berkeley, class of 2023. Nice to meet you! As of now, I am looking for an intern position in a variety of technical fields in CS and math, varying from software engineering to machine learning to quant trading. If you want to learn more about me or contact me, check out my social links above!
             </p>
-            <p>
-                This site's still under construction (I started like 3 days ago...), but I've been putting my foot on the pedal, so hopefully this will mostly be done by the end of November (doable in 2 days?). Anyways, feel free to look around, my social links (Github, Linkedin, resume, etc) are working but everything under may be incomplete. 
-            </p>
         </div>
     )
 }
