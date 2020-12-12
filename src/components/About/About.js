539f079e00bdb8ee9703aea6a0a6053f402f1f14
patwang123 <patrickwang86@gmail.com>
2020-12-12 16:20:38 -0500

    beginning of profile and skills

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,6 +8,20 @@ export default function About () {
             <p>
                 I'm Patrick Wang, a sophomore at UC Berkeley, class of 2023. Nice to meet you! As of now, I am looking for an intern position in a variety of technical fields in CS and math, varying from software engineering to machine learning to quant trading. If you want to learn more about me or contact me, check out my social links above!
             </p>
+            <div className='double-column'>
+                <div className='column'>
+                    <h3>Profile</h3>
+                    <div className='profile'>
+                        To be implemented!
+                    </div>
+                </div>
+                <div className='column'>
+                    <h3>Skills</h3>
+                    <div className='skills'>
+                        To be implemented!
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
